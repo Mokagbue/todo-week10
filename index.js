a4d6afe103ba1ec27ab0d91d3683b977fbb29f25
Michelle Okagbue <umbralcompendium@gmail.com>
2023-04-10 02:46:50 -0400

    inital setup, tutorial functioning

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ document.getElementById('add').addEventListener('click', () =>{
     row.setAttribute('id', `item-${id}`);
     row.insertCell(0).innerHTML = document.getElementById('new-task').value;
     row.insertCell(1).innerHTML = `${createdDate.getMonth() + 1}-${createdDate.getDate()}-${createdDate.getFullYear()}`;
-    row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
-    row.insertCell(3).innerHTML = document.getElementById('new-end-date').value;
-    let actions = row.insertCell(4);
+    let actions = row.insertCell(2);
     actions.appendChild(deleteButton(id++));
     document.getElementById('new-task').value = '';
 })
@@ -16,7 +14,8 @@ function deleteButton(id) {
     let btn = document.createElement('button');
     btn.className = 'btn btn-danger';
     btn.id = id;
-    btn.innerHTML = 'Delete';
+    btn.style = `font-family:${'Permanent Marker'}, cursive;`
+    btn.innerHTML = 'Completed';
     btn.onclick = () => {
         console.log(`Deleted row: item-${id}`);
         let toDelete = document.getElementById(`item-${id}`);
